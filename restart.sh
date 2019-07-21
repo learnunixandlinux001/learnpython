b40c293aa07f2b1648e8208994512f3c6c8ebd5c
Learn Python <learnunixandlinux001@gmail.com>
2019-07-21 09:44:36 +0530

    logs.out

--- restart.sh
+++ restart.sh
@@ -12,6 +12,6 @@ ps -ef|grep -v grep|grep HelloSel|awk '{print $2}'|xargs kill -9
 ps -ef|grep -v grep|grep tor-browser|awk '{print $2}'|xargs kill -9
 echo "killed"
 echo "Starting up now...."
-python3 HelloSel.py 1>logs.out & 
+python3 HelloSel.py 1>logs.out 2>logs.out 0>logs.out & 
 echo "...success"
 fi 
