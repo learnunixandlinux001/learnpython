70cd23903de5e26d0155cb43c5d62c861ab72ecb
Ubuntu <ubuntu@ip-172-31-94-83.ec2.internal>
2019-07-21 09:28:06 +0000

    added init

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt update
+sudo apt install -y python3
+sudo apt install -y python3-pip
+pip3 install selenium
+pip3 install tbselenium
+pip3 install pymysql
+pip3 install unidecode
+sudo apt install -y tor
