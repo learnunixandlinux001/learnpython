ba9b459380f731ddb2fe143f8fbf4cc0964604de
Learn Python <learnunixandlinux001@gmail.com>
2019-07-24 12:53:03 +0530

    clean tmp during dumb restart

--- dumbrestart.sh
+++ dumbrestart.sh
@@ -9,6 +9,10 @@ echo "Needs a restart!"
 ps -ef|grep -v grep|grep HelloSel|awk '{print $2}'|xargs kill -9
 ps -ef|grep -v grep|grep tor-browser|awk '{print $2}'|xargs kill -9
 echo "Dumbly killed"
+echo "Freeing up tmp..."
+cd /tmp
+sudo rm -rf *
+cd ~/learnpython
 echo "Starting up now...."
 python3 HelloSel.py 1>logs.out 2>logs.out 0>logs.out & 
 echo "...success"
