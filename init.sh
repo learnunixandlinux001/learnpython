6731376671d735eaf79516ba266569d3bfe269f3
learnunixandlinux001 <53087711+learnunixandlinux001@users.noreply.github.com>
2019-12-19 23:16:35 +0530

    Tor version upgrade

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~/Downloads
-wget https://www.torproject.org/dist/torbrowser/8.5.5/tor-browser-linux64-8.5.5_en-US.tar.xz
-wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
-mkdir ~/Downloads/tor-browser-linux64-8.5.5_en-US
-tar -xvf tor-browser-linux64-8.5.5_en-US.tar.xz -C ~/Downloads/tor-browser-linux64-8.5.5_en-US  
+wget https://www.torproject.org/dist/torbrowser/9.0.2/tor-browser-linux64-9.0.2_en-US.tar.xz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+mkdir ~/Downloads/tor-browser-linux64-9.0.2_en-US
+tar -xvf tor-browser-linux64-9.0.2_en-US.tar.xz -C ~/Downloads/tor-browser-linux64-9.0.2_en-US  
 mkdir ~/Downloads/geckodriver-v0.24.0-linux64
 tar -xvf geckodriver-v0.24.0-linux64.tar.gz -C ~/Downloads/geckodriver-v0.24.0-linux64
 cd ~/learnpython
@@ -19,6 +19,6 @@ pip3 install tbselenium
 pip3 install pymysql
 pip3 install unidecode
 sudo apt install -y tor
-sudo cp ~/Downloads/geckodriver-v0.24.0-linux64/geckodriver /usr/local/bin
+sudo cp ~/Downloads/geckodriver-v0.26.0-linux64/geckodriver /usr/local/bin
 sudo chmod 777 /usr/local/bin/geckodriver
 crontab cronconfig.txt
